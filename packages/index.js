db7db58ae31a1b5acd621d971ee70cbde8276392
Vinicius Azevedo <vinicius.azevedo@convenia.com.br>
2019-05-07 17:21:28 -0300

    init

--- packages/index.js
+++ packages/index.js
@@ -1,8 +1,2 @@
 export { default as ACard } from './ACard'
 export { default as ATitle } from './ATitle'
-
-// export const ACard = () => import('./ACard')
-// export const ATitle = () => import('./ATitle')
-
-// export * from './ACard'
-// export * from './ATitle'
