6841378a9fd4045d378270a0ef43007cccc543a9
Vinicius Azevedo <vinicius.azevedo@convenia.com.br>
2019-05-07 17:17:54 -0300

    init

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import VuePlugin from 'rollup-plugin-vue'
+import { terser } from 'rollup-plugin-terser'
+
+export default {
+  input: 'index.js',
+  output: {
+    file: 'dist/bundle-all.js',
+    format: 'cjs',
+    sourcemap: true
+  },
+  plugins: [
+    resolve(),
+    commonjs(),
+    VuePlugin(),
+    terser()
+  ]
+}
