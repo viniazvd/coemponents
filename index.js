a58653d37ffca8e28f2db2b6867a278945768872
Vinicius Azevedo <vinicius.azevedo@convenia.com.br>
2019-05-08 15:02:46 -0300

    init

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import * as components from './packages/index.js'
+const ACard = () => import('@coemponents/card')
+const ATitle = () => import('@coemponents/title')
 
 const install = Vue => Object
   .entries(components)
   .forEach(([name, component]) => Vue.component(name, component))
 
-export * from './packages/index.js'
+// export * from './packages/index.js'
+
+export {
+  ACard,
+  ATitle
+}
 
 export default install
--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ import { terser } from 'rollup-plugin-terser'
 export default {
   input: 'index.js',
   output: {
-    file: 'dist/bundle.js',
+    dir: 'dist',
     format: 'cjs',
     sourcemap: true
   },
