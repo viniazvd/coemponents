d325e0c9160a415c45f1f05319d611a05f4563b6
Vinicius Azevedo <viniazvd@gmail.com>
2019-05-27 17:17:22 -0300

    :art: improves

--- packages/ACard/rollup.config.js
+++ packages/ACard/rollup.config.js
@@ -1,11 +1,11 @@
-// import postcss from 'postcss'
+import postcss from 'postcss'
 import vue from 'rollup-plugin-vue'
-// import sass from 'rollup-plugin-sass'
+import sass from 'rollup-plugin-sass'
 // import css from 'rollup-plugin-css-only'
 import autoprefixer from 'autoprefixer'
-// import commonjs from 'rollup-plugin-commonjs'
-// import { terser } from 'rollup-plugin-terser'
-// import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import { terser } from 'rollup-plugin-terser'
+import resolve from 'rollup-plugin-node-resolve'
 
 export default {
   input: 'src/index.js',
@@ -15,24 +15,27 @@ export default {
     sourcemap: true
   },
   plugins: [
-    // resolve(),
-    // commonjs(),
+    resolve(),
+    commonjs(),
     // css({ output: 'dist/card.css' }),
     vue({
-      css: false,
-      style: {
-        trim: false,
-        postcssPlugins: [
-          autoprefixer({ browsers: ['> 0.00001%'] })
-        ]
-      }
+      // css: false,
+      // style: {
+      //   trim: false,
+      //   postcssPlugins: [
+      //     autoprefixer({ browsers: ['> 0.00001%'] })
+      //   ]
+      // }
+    }),
+    terser(),
+    sass({
+      output: 'dist/card.css',
+      options: {
+        includePaths: ['node_modules']
+      },
+      processor: css => postcss([autoprefixer])
+        .process(css)
+        .then(result => result.css)
     })
-    // terser(),
-    // sass({
-    //   output: 'dist/card.css',
-    //   processor: css => postcss([autoprefixer])
-    //     .process(css)
-    //     .then(result => result.css)
-    // })
   ]
 }
