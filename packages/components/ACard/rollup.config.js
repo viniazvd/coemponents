af412a5f1f69b405a1fe0c3026af402e2af7ea5b
Vinicius Azevedo <viniazvd@gmail.com>
2019-05-28 23:50:31 -0300

    :zap: maybe works

--- packages/components/ACard/rollup.config.js
+++ packages/components/ACard/rollup.config.js
@@ -1,21 +1,38 @@
-import vue from 'rollup-plugin-vue'
-import css from 'rollup-plugin-css-only'
-import commonjs from 'rollup-plugin-commonjs'
-import { terser } from 'rollup-plugin-terser'
 import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+
+import vue from 'rollup-plugin-vue'
+
+import sass from 'rollup-plugin-sass'
+import css from 'rollup-plugin-css-only'
+// import { terser } from 'rollup-plugin-terser'
+
+import autoprefixer from 'autoprefixer'
+import postcss from 'postcss'
 
 export default {
   input: 'src/index.js',
   output: {
-    file: 'dist/card.js',
+    name: 'ACard',
+    file: 'dist/js/card.js',
     format: 'cjs',
     sourcemap: true
   },
   plugins: [
     resolve(),
     commonjs(),
-    css({ output: 'dist/card.css' }),
     vue({ css: false }),
-    terser()
+    sass({
+      output: 'dist/css/card.css',
+      // Processor will be called with two arguments:
+      // - style: the compiled css
+      // - id: import id
+      processor: css => postcss([autoprefixer])
+        .process(css)
+        .then(result => result.css)
+    }),
+    css({ output: false }),
+    uglify()
+    // terser()
   ]
 }
